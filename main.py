99dd5063ded3c00774183c8291fd8f526f28d0c6
Malderok <cardaju@gmail.com>
2025-06-08 16:04:54 +0200

    Reporte creado y funcionando

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+from stats import count_characters
+from stats import display_count
 from stats import count_book_word
-
+from stats import get_book_text
 
 def main():
 
-    count_book_word("/home/curbal/github.com/Malderok/bookbot/books/frankenstein.txt")
+    f = "/home/curbal/github.com/Malderok/bookbot/books/frankenstein.txt"
 
-    
+    conteo = count_characters(f)
+    total_words = count_book_word(f)
+
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for line in display_count(conteo):
+        print(f"{line["char"]}: {line["num"]}")
+
+    return
+
 
 main()
--- stats.py
+++ stats.py
@@ -4,12 +4,60 @@ def count_book_word(file_path):
 
         file_contents = f.read()
         words_string = file_contents.split()
-        print(f"{len(words_string)} words found in the document")
+        #print(f"{len(words_string)} words found in the document")
+        return len(words_string)
 
 
 def get_book_text(file_path):
     
     with open(file_path) as f:
 
-        file_contents = f.read()
+        file_contents = file_path.read()
         print(file_contents)
+
+
+
+def count_characters(file_path):
+    character_count = {}
+
+    with open(file_path) as f:
+
+        file_contents = f.read()
+
+        file_contents = file_contents.lower()
+
+        for character in file_contents:
+            
+
+            if character in character_count:
+
+                character_count[character] += 1
+
+            else:
+
+                character_count[character] = 1
+                
+    return character_count
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def display_count(character_count):
+
+    dictionaty_list = []
+
+
+    for character in character_count:
+
+
+        if character.isalpha():
+
+            dictionaty_list.append({"char": character , "num" : character_count[character]})
+
+
+
+    dictionaty_list.sort(reverse=True , key=sort_on )
+
+
+    return dictionaty_list
